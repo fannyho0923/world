ec2a1d0b96954779b10cbedcb6917cbc84cadfb9
howeihan <fannyho0923@gmail.com>
2021-09-11 21:33:55 +0800

    about內容完成

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,19 +1,107 @@
 <template>
   <div>
     <Nav />
-    <Bg />
+    <Bg class="bg" />
+    <div class="contentBox">
+      <Content
+        class="content"
+        :data="item"
+        v-for="(item, index) in datas"
+        :key="index"
+        :i="index"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import Nav from "@/components/Nav";
 import Bg from "@/components/Bg";
+import Content from "@/components/Content";
 export default {
+  data() {
+    return {
+      datas: [
+        {
+          title: "Full-Stack Developer",
+          time: "2021/5/1 to 2021/7/31",
+          skills: [
+            {
+              name: "AWS",
+              url: "https://aws.amazon.com/?nc1=h_ls",
+              img: require("@/assets/aws.png"),
+              desc: [
+                "Lambda serverless With NodeJS \+ APIGateway",
+                "S3 \+ Cloudfont \+ Certification Manager \+ Godaddy For Web Hosting",
+                "postgreSQL DataBase"
+              ]
+            },
+            {
+              name: "React",
+              url: "https://reactjs.org/",
+              img: require("@/assets/react.png"),
+              desc: ["react + redux + redux-saga", "Material-UI"]
+            }
+          ]
+        },
+        {
+          title: "Backend Developer",
+          time: "2021/1/1 to 2021/3/31",
+          skills: [
+            {
+              name: "AWS",
+              url: "https://aws.amazon.com/?nc1=h_ls",
+              img: require("@/assets/aws.png"),
+              desc: [
+                "Lambda serverless With Python + APIGateway",
+                "TCP Server With NodeJS",
+                "DynamoDB",
+                "S3 For Data storage",
+                "SES"
+              ]
+            },
+            {
+              name: "Firebase",
+              url: "https://firebase.google.com/",
+              img: require("@/assets/firebase.png"),
+              desc: ["Web Hosting", "FCM Notification"]
+            },
+            {
+              name: "Other",
+              url: "https://0.0.1.148/",
+              img: require("@/assets/other.png"),
+              desc: ["every8d SMS Notification"]
+            }
+          ]
+        }
+      ]
+    };
+  },
   components: {
     Nav,
-    Bg
+    Bg,
+    Content
   }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.contentBox {
+  width: 65%;
+  height: auto;
+  margin: 0 auto;
+}
+
+.content {
+  width: 100%;
+}
+.bg {
+  position: absolute;
+  top: 0;
+}
+@media (max-width: 576px) {
+  .contentBox {
+    width: 90%;
+  }
+}
+</style>
---
+++ src/components/Content.vue
@@ -0,0 +1,131 @@
+<template>
+  <div
+    :class="{
+      left: i % 2 == 0,
+      right: i % 2 == 1
+    }"
+    class="body"
+  >
+    <main class="main">
+      <section class="top">
+        <div class="tit-top">{{ data.title }}</div>
+        <div>{{ data.time }}</div>
+      </section>
+      <section class="mid">
+        <label class="tit-mid">What have I Do???</label>
+        <div class="skills" v-for="(item, index) in data.skills" :key="index">
+          <div class="skillBox">
+            <a :href="data.skills[index].url" target="_blank">{{
+              data.skills[index].name
+            }}</a>
+            <img :src="data.skills[index].img" alt="" />
+          </div>
+          <p class="skp" v-for="(item, i) in data.skills[index].desc" :key="i">
+            {{ data.skills[index].desc[i] }}
+          </p>
+        </div>
+        <a class="more" href="">More Reference</a>
+      </section>
+      <section class="bottom">
+        <label>jasonMini:~ jasonsiuaw$</label>
+        <span>|...</span>
+      </section>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isActive: true
+    };
+  },
+  props: {
+    data: { type: Object },
+    i: { type: Number }
+  }
+};
+</script>
+
+<style scoped>
+* {
+  font-family: "VT323";
+  color: rgb(74, 246, 38);
+}
+img {
+  width: 7%;
+  height: 7%;
+}
+.left {
+  display: flex;
+}
+.right {
+  display: flex;
+  flex-direction: row-reverse;
+}
+
+.body {
+  width: 100%;
+  border-radius: calc(0.3rem - 1px);
+  margin: 3rem 0;
+}
+.main {
+  width: 100%;
+  background-color: rgb(33, 37, 41);
+}
+.tit-top {
+  font-size: 24px;
+}
+.tit-mid {
+  font-size: 25px;
+}
+.top {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem;
+  border-bottom: 1px solid #dee2e6;
+}
+.mid {
+  padding: 1rem;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem;
+  border-bottom: 1px solid #dee2e6;
+}
+.bottom {
+  padding: 0.3rem 0.75rem;
+}
+.bottom label {
+  font-size: 18px;
+}
+.more {
+  display: block;
+  font-size: 20px;
+  color: rgb(0, 95, 255);
+  margin-top: 2rem;
+}
+.skillBox {
+  font-size: 20px;
+  display: flex;
+  align-items: center;
+  padding: 0.75rem 0;
+}
+.skp + .skp {
+  margin-top: 0.75rem;
+}
+span {
+  animation: blink 1s infinite;
+}
+@keyframes blink {
+  to {
+    color: transparent;
+  }
+}
+@media (min-width: 576px) {
+  .main {
+    max-width: 500px !important;
+  }
+}
+</style>
